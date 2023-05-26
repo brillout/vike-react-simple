d7c83c760630f152616d63b0d00e92d2bb75da9c
Romuald Brillout <git@brillout.com>
2023-05-26 15:53:49 +0200

    fix: use latest vps non-header file import format

--- +config.mjs
+++ +config.mjs
@@ -1,6 +1,6 @@
 export default {
-  onRenderHtmlPath: 'vike-react-simple/onRenderHtml',
-  onRenderClientPath: 'vike-react-simple/onRenderClient',
+  onRenderHtmlPath: 'import:vike-react-simple/onRenderHtml',
+  onRenderClientPath: 'import:vike-react-simple/onRenderClient',
   meta: {
     Layout: {
       env: 'server-and-client'
