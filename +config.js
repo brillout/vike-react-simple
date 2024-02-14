f8e95a9bb8a774bd3ba13aff33d03b6d3af1e79f
Romuald Brillout <git@brillout.com>
2024-02-14 16:35:46 +0100

    fix: fix meta

--- +config.js
+++ +config.js
@@ -1,14 +1,16 @@
 export default {
   name: 'vike-react-simple',
+
   onRenderHtml: 'import:vike-react-simple/onRenderHtml',
   onRenderClient: 'import:vike-react-simple/onRenderClient',
+
   meta: {
     Layout: {
       env: { server: true, client: true }
+    },
+    // TODO: remove once Vike's new version is released
+    name: {
+      env: { config: true }
     }
-  },
-  // TODO: remove once Vike's new version is released
-  name: {
-    env: { config: true }
   }
 }
