4af6b707959b57eaef5398331890b4eee4ce03ed
Romuald Brillout <git@brillout.com>
2024-02-14 16:33:00 +0100

    fix: set Vike extension name

--- +config.js
+++ +config.js
@@ -1,9 +1,14 @@
 export default {
+  name: 'vike-react-simple',
   onRenderHtml: 'import:vike-react-simple/onRenderHtml',
   onRenderClient: 'import:vike-react-simple/onRenderClient',
   meta: {
     Layout: {
       env: { server: true, client: true }
     }
+  },
+  // TODO: remove once Vike's new version is released
+  name: {
+    env: { config: true }
   }
 }
